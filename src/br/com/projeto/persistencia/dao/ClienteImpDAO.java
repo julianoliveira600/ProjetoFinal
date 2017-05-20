582680280ced0c97aeaba35e5ec4cc8d697d6e1e
Vinicius Bord <viniciusbord9@gmail>
2017-05-20 13:42:56 -0300

    ajustes no clienteImpDAO

--- src/br/com/projeto/persistencia/dao/ClienteImpDAO.java
+++ src/br/com/projeto/persistencia/dao/ClienteImpDAO.java
@@ -43,7 +43,7 @@ public class ClienteImpDAO extends BaseImpDAO implements BaseDAO<Cliente>  {
 
     @Override
     public int edit(Cliente cliente) throws PersistenciaException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
